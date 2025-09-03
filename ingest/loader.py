f31391fb7fdf3f416b48b88bd2b67b3c9dbe4bbb
SeokHyun Moon <seokhyunmoon0129@gmail.com>
2025-09-03 14:03:45 +0800

    adding Ingestion Pipeline

---
+++ ingest/loader.py
@@ -0,0 +1,36 @@
+from typing import List
+import pathlib
+from langchain_core.documents import Document
+from langchain_community.document_loaders import PyMuPDFLoader, TextLoader
+
+def load_docs(file_paths: List[str]) -> List[Document]:
+    """
+    Load documents from the given file paths.
+    Supports PDF and TXT files.
+    """
+    
+    all_docs : List[Document] = []
+    for file_path in file_paths:
+        path = pathlib.Path(file_path)
+        
+        if not path.exists():
+            print(f"File {file_path} does not exist.")
+            continue
+        
+        if path.suffix.lower() == '.pdf':
+            loader = PyMuPDFLoader(file_path)
+        elif path.suffix.lower() == '.txt':
+            loader = TextLoader(file_path, encoding='utf8')
+        else:
+            print(f"Unsupported file type: {file_path}.")
+            continue
+        
+        docs = loader.load()
+        
+        
+        # for d in docs:
+        
+        
+        all_docs.extend(docs)
+    
+    return all_docs
---
+++ ingest/splitter.py
@@ -0,0 +1,24 @@
+from typing import List
+from langchain_core.documents import Document
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+_CHUNK_SIZE = 1000
+_CHUNK_OVERLAP = 150
+
+_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=_CHUNK_SIZE,
+    chunk_overlap=_CHUNK_OVERLAP,
+)
+
+def split_docs(docs: List[Document]) -> List[Document]:
+    """
+    Split documents into smaller chunks.
+    """
+    # all_splitted_docs : List[Document] = []
+    # for doc in docs:
+    #     splitted_docs = _splitter.split_documents([doc])
+    #     all_splitted_docs.extend(splitted_docs)
+    
+    # return all_splitted_docs
+    
+    return _splitter.split_documents(docs)
