6066927635ca0000759b077baa8e36aaf394c9fb
SeokHyun Moon <seokhyunmoon0129@gmail.com>
2025-10-24 15:37:03 +0800

    Reset

--- ingest/embedder.py
+++
@@ -1,9 +0,0 @@
-from langchain_huggingface import HuggingFaceEmbeddings
-
-_EMB = None
-
-def get_embeddings():
-    global _EMB
-    if _EMB is None:
-        _EMB = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-    return _EMB
